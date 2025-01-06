a235f2a6c428ae2f890c7720c9edf41a943ab6e2
Kazuhiro Ishibashi <105427495+Kaz-Ishibashi@users.noreply.github.com>
2025-01-07 01:45:40 +0900

    白紙に戻っていたファイルを復元

--- azure_translate.ps1
+++ azure_translate.ps1
@@ -1,1 +1,10 @@
+$output = "sbj\" + $Args[0] + "_translated.txt"
+Write-Output "translating $Args[0].txt to Japanese..."
 
+python azure.py $Args[0]
+
+#Start-Sleep -s 2
+#Write-Output "translation completed."
+
+# クリップボードにコピー
+Get-Content -Encoding Shift-JIS $output|Set-clipboard
